d8522ebc75f49ef8ebf54bf8a273ce44c35960a4
mike-karl <mike.karl1196@gmail.com>
2022-04-06 16:51:30 -0500

    wrapped my icon tags in div tags and made the divs the clickable element. This allowed all the timer and content test to pass. Added breakTime.mp3 file and made it so when the tiemr reaches 0 the audio plays for 1 sec then is paused and rewound. Updated handleReset so that it would pause the audio and rewind it as well. The audio is played using the html audio tag and the useRef hook. The app now passes all tests!

--- src/App.js
+++ src/App.js
@@ -1,51 +1,40 @@
 import { ImArrowUp, ImArrowDown } from 'react-icons/im';
 import { FaPlay, FaPause, FaRedo } from 'react-icons/fa';
-import { useCallback, useEffect, useState} from 'react';
+import { useEffect, useRef, useState } from 'react';
+import soundFile from './breakTime.mp3'; 
 import './App.css';
 
 function App() {
   //initialize state here:
-  const [ brkLength, setBrkLength ] = useState(5);
+  const [brkLength, setBrkLength ] = useState(5);
   const [sessionLength, setSessionLength] = useState(25);
+  const [timerType, setTimerType] = useState('session');
   const [timerState, setTimerState] = useState('paused');
-  const [timerType, setTimerType ] = useState('session');
-  const [timer, setTimer] = useState(1500);
+  const [timer, setTimer] = useState(25 * 60);
   const [startTime, setStartTime] = useState(null);
   const [totalTime, setTotalTime] = useState(null);
+  // initialize useRef hook for audio beep
+  const beep = useRef(null);
 
-  /*a callback hook that is used in the useEffect hook to change the timer type
+  /* This is the timer logic. The timer accounts for drift and changes 
+  the interval based on the previous runs drift. This means the timer 
+  will always lag slightly behind in miliseconds but the drift will 
+  never get out of control, thus makaing the timer far more accurate.*/
+ 
+  useEffect(() => {
+
+    /* Function used in the useEffect hook to change the timer type
    and set the timer to the length of the selected timerType (break or session).*/
-  const changeTimerType = useCallback(() => {
-    if (timerType === 'session') {
+   const changeTimerType = () => { 
+   if (timerType === 'session') {
       setTimerType('break');
       setTimer(brkLength * 60);
     } else {
       setTimerType('session');
       setTimer(sessionLength * 60);
     }
-  }, [timerType, brkLength, sessionLength]);
-  /*this function formats the timer(in seconds) to display as mm:ss */
-  const timerFormat = (timer) => {
-    let minutes = Math.floor(timer / 60);
-    let seconds = timer - minutes * 60;
-    let formattedTime = minutes + ':' + seconds;
-    if (minutes < 10) {
-    seconds >= 10 ? formattedTime = '0' + minutes + ':' + seconds : formattedTime = '0' + minutes + ':0' + seconds;
-     return formattedTime;
-    } else {
-      seconds >= 10 ? formattedTime = minutes + ':' + seconds : formattedTime = minutes + ':0' + seconds;
-      return formattedTime;
-    }
-  } 
-  /*Variable used to display the timer in mm:ss format. Calls the timerFormat function and passes 
-  in the timer state variable which is in seconds and formats it to mm:ss*/
-  const timerDisplay = timerFormat(timer);
-
-  /* This is the timer logic. The timer accounts for drift and changes 
-  the interval based on the previous runs drift. This means the timer 
-  will always lag slightly behind in miliseconds but the drift will 
-  never get out of control, thus makaing the timer far more accurate.*/
-  useEffect(() => {
+  }
+    console.log('rendered');
     let interval = null;
     if (timerState === 'running' && timer > 0) {
       if (startTime === null) {
@@ -55,114 +44,137 @@ function App() {
       interval = setInterval(() => {
         setTotalTime(t => t + 1000 );
         setTimer( timer => timer - 1);
-      console.log('original start time: ' + startTime);
-      console.log('current total time: ' + totalTime);
       }, 1000 - (Date.now() - totalTime)); 
-      console.log('totalTime: ' + totalTime);
-      console.log('drift: ' + (Date.now() - totalTime))
-      console.log('New time interval:' + ((1000 - (Date.now() - totalTime))));
     }
     } else if (timerState === 'paused' && timer !== 0) {
       clearInterval(interval);
       setStartTime(null);
     } else if (timerState === 'running' && timer === 0) {
-      setTimerState('paused');
       clearInterval(interval);
-      console.log(timerType);
-    } else if (timerState === 'paused' && timer === 0) {
-      console.log('Im working');
-      const pause = setTimeout(() => {
+      setStartTime(null);
+      beep.current.play();
+      // creates pause effect for timer, also stops and rewinds audio after 1 second of playing;
+      let pause = setTimeout(() => {
         changeTimerType();
-        setTimerState('running');
+        beep.current.pause();
+        beep.current.load();
         console.log(timerType);
-      }, 3000);
+      }, 1000);
       return () => clearTimeout(pause);
-    }
-    
+    } 
     return () => clearInterval(interval);
-  }, [timerState, timer, changeTimerType, timerType, startTime, totalTime])
+  }, [timerState, timer, timerType, startTime, totalTime, brkLength, sessionLength]);
+
   /*When the timerState is 'paused' increments the sessionLength or brkLength 
   depending on which element is clicked by 1 minute. If the timerType is actively displayed 
   the timer time will be set to the new break/session length */
   function handleTimeIncrement(e) {
     if (timerState === 'paused') {
       if (e.currentTarget.id === 'break-increment' && brkLength < 60) {
-        setBrkLength(brkLength + 1);
+        setBrkLength(prevLength => prevLength + 1);
         if (timerType === 'break') {
           setTimer((brkLength + 1) * 60);
         }
       } else if (e.currentTarget.id === 'session-increment'  && sessionLength < 60) {
-        setSessionLength(sessionLength + 1)
+        setSessionLength(prevLength => prevLength + 1);
         if (timerType === 'session') {
           setTimer((sessionLength + 1) * 60);
         }
       }
     } 
 }
+
   /*When the timerState is 'paused' decrements the sessionLength or brkLength 
   depending on which element is clicked by 1 minute. If the timerType is actively displayed 
   the timer time will be set to the new break/session length */
   function handleTimeDecrement(e) {
     if (timerState === 'paused') {
       if (e.currentTarget.id === 'break-decrement' && brkLength > 1) {
-        setBrkLength(brkLength - 1);
+        setBrkLength(prevLength => prevLength - 1);
         if (timerType === 'break') {
           setTimer((brkLength - 1) * 60);
         }
       } else if (e.currentTarget.id === 'session-decrement' && sessionLength > 1) {
-        setSessionLength(sessionLength - 1)
+        setSessionLength(prevLength => prevLength - 1)
         if (timerType === 'session') {
           setTimer((sessionLength - 1) * 60);
         }
-      } else {
-        return;
-      }
-    } else {
-      return;
-    }
+      } 
+    } 
   }
   // resets the timer to initial state.
   function handleReset() {
+    setTimer(1500);
     setTimerState('paused');
     setBrkLength(5);
     setSessionLength(25);
     setTimerType('session');
-    setTimer(1500);
+    beep.current.pause();
+    beep.current.load();
   }
+
   // this function switches the state of the timer from 'paused' to 'running' or 'running' to 'paused' 
-  function handleTimerState() {
+   function handleTimerState() {
     if (timerState === 'paused') {
       setTimerState('running')
+      console.log(timerState);
      } else if (timerState === 'running'){
        setTimerState('paused')
+       console.log(timerState);
      }
   }
+    /*this function formats the timer(in seconds) to display as mm:ss */
+    const timerFormat = (time) => {
+      let minutes = Math.floor(timer / 60);
+      let seconds = time - minutes * 60;
+      let formattedTime;
+      console.log(seconds);
+      if (minutes < 10) {
+      seconds >= 10 ? formattedTime = '0' + minutes + ':' + seconds : formattedTime = '0' + minutes + ':0' + seconds;
+        return formattedTime;
+      } else {
+        seconds >= 10 ? formattedTime = minutes + ':' + seconds : formattedTime = minutes + ':0' + seconds;
+        return formattedTime;
+      }
+    } 
+  
 
   return (
     <div className="App">
       <h1>25 + 5 Clock</h1>
       <div>
         <h2 id="break-label">Break Length</h2>
-        <ImArrowUp id="break-increment" onClick={handleTimeIncrement}/> 
+        <div id="break-increment" onClick={handleTimeIncrement}>
+          <ImArrowUp /> 
+        </div>
         <div id="break-length">{brkLength}</div>
-        <ImArrowDown id="break-decrement" onClick={handleTimeDecrement}/>
+        <div id="break-decrement" onClick={handleTimeDecrement}>
+          <ImArrowDown/>
+        </div>
       </div>
 
       <div>
         <h2 id="session-label">Session Length</h2>
-        <ImArrowUp id="session-increment" onClick={handleTimeIncrement}/> 
-        <div id="session-length">{sessionLength}</div>
-        <ImArrowDown id="session-decrement" onClick={handleTimeDecrement}/>
+        <div id="session-increment" onClick={handleTimeIncrement}>
+          <ImArrowUp /> 
+        </div>
+        <div id="session-length" >{sessionLength}</div>
+        <div id="session-decrement" onClick={handleTimeDecrement}>
+          <ImArrowDown />
+        </div>
       </div>
 
       <div>
         <h2 id="timer-label">{timerType}</h2>
-        <h2 id="time-left">{timerDisplay}</h2>
+        <h2 id="time-left">{timerFormat(timer)}</h2>
+        <audio id='beep' ref={beep} src={soundFile} type="audio/mpeg"></audio>
         <div id="start_stop" onClick={handleTimerState}>
           <FaPlay />
           <FaPause />
         </div >
-        <FaRedo id="reset" onClick={handleReset}/>
+        <div id="reset" onClick={handleReset}>
+          <FaRedo />
+        </div>
       </div>
 
     </div>
